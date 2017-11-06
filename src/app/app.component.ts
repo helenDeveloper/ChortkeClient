e76da6f14b3bffde698aedb69303c5a5c8993cb5
helenDeveloper <startpoint1987@gmail.com>
2017-11-06 16:19:49 +0330

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {HeroService} from "./hero.service";
+
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers:[HeroService]
+})
+export class AppComponent implements  OnInit {
+message='this is a message from client';
+
+
+
+  constructor(private hservice:HeroService) {};
+
+  ngOnInit()
+  {
+    this.hservice.getMessage().then(msg => this.message=msg);
+    alert(this.message);
+  }
+
+  title = 'app';
+}
+
+
+
---
+++ src/app/hero.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export  class HeroService {
+
+
+  private baseUrl='api/hi';
+
+  constructor(private http: Http){}
+
+getMessage():Promise<string>
+{
+
+  return  this.http.get(this.baseUrl).toPromise().then(res => res.json().data).catch(this.handleError);
+
+}
+
+private handleError(error :any)
+{
+
+
+
+
+  console.error("an error accured",error);
+  return Promise.reject(error.message || error);
+}
+
+
+
+}
